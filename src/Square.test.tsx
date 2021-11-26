93e50c6a97fd685fbc281a515d70075cf7d97903
Jorge Gomez <jorge.gomez@thoughtworks.com>
2021-11-26 11:29:24 +0100

    Added Square component

--- src/Square.test.tsx
+++ src/Square.test.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Square } from "./Square";
 
 test("it renders correctly when nobody clicked on it", () => {
-  const result = render(<Square />);
-  const element = result.getByTestId("square");
+  const result = render(<Square position={1} onClick={() => {}} />);
+  const element = result.getByTestId("square-1");
   expect(element).toHaveTextContent("");
 });
 
 test("it renders correctly when player 1 clicked on it", () => {
-  const result = render(<Square player="X" />);
-  const element = result.getByTestId("square");
+  const result = render(<Square position={1} player="X" onClick={() => {}} />);
+  const element = result.getByTestId("square-1");
   expect(element).toHaveTextContent("X");
+});
+
+test("it should call click handler when it does not have value", () => {
+  const onClick = jest.fn();
+  const result = render(<Square position={1} onClick={onClick} />);
+  const element = result.getByTestId("square-1");
+  fireEvent.click(element);
+  expect(onClick).toBeCalled();
+});
+
+test("it should not call click handler when it has value", () => {
+  const onClick = jest.fn();
+  const result = render(<Square position={1} onClick={onClick} player={"X"} />);
+  const element = result.getByTestId("square-1");
+  fireEvent.click(element);
+  expect(onClick).not.toBeCalled();
 });
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,3 +1,17 @@
-export const Square = () => {
-  return <div data-testid="square"></div>;
+import React from 'react';
+
+export type SquareProps = {
+  position: number;
+  onClick: () => void;
+  player?: string;
+}
+
+export const Square: React.FC<SquareProps> = ({ player, position, onClick }) => {
+  const clickHandler = () => {
+    if (!player) {
+      onClick();
+    }
+  }
+
+  return <div data-testid={`square-${position}`} onClick={clickHandler}>{player}</div>;
 };
